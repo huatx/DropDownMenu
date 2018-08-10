7deb49af359426a10086384cbea6b5f6420f51de
葱花 <xiaoyao_2009@126.com>
2018-08-10 15:23:57 +0800

    仿美团下拉筛选列表

--- app/src/main/java/com/example/dropdownmenu/MainActivity.java
+++ app/src/main/java/com/example/dropdownmenu/MainActivity.java
@@ -3,21 +3,42 @@ package com.example.dropdownmenu;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.TextView;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     private DropDownMenuView dropDownMenu;
+    private GridView constellation;
+    private ConstellationAdapter constellationAdapter;
+    private String constellations[] = {"不限", "白羊座", "金牛座", "双子座", "巨蟹座", "狮子座", "处女座", "天秤座", "天蝎座", "射手座", "摩羯座", "水瓶座", "双鱼座"};
+    private int constellationPosition = 0;
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        constellationAdapter = new ConstellationAdapter(this, Arrays.asList(constellations));
+        dropDownMenu = findViewById(R.id.dropDownMenu);
+        constellation = findViewById(R.id.constellation);
+        constellation.setAdapter(constellationAdapter);
 
-        dropDownMenu = (DropDownMenuView)findViewById(R.id.dropDownMenu);
-        TextView textView = (TextView) findViewById(R.id.view_filter);
+        constellation.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                constellationAdapter.setCheckItem(position);
+                constellationPosition = position;
+                dropDownMenu.close();
+            }
+        });
+        TextView textView = findViewById(R.id.view_filter);
         textView.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-                if(!dropDownMenu.isOpen()){
+            @Override
+            public void onClick(View v) {
+                if (!dropDownMenu.isOpen()) {
                     dropDownMenu.open();
                 }
             }
